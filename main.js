2def3539e1f1f9f47717ae2f589509d01ffab524
samyak281009 <142726967+samyak281009@users.noreply.github.com>
2023-08-27 15:56:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+function preload() {
+
+}
+function setup(){
+    canvas=createCavas(600,700)
+    canvas.position(70,140)
+    createCavas(1500,300)
+}
+function draw(){
+    
+}
+function take_snapshot(){
+    save("frame.png");
+    }
